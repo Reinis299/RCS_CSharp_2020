88aa7fa5dd794581e0a2a57388f296e02649bf63
Martins <p998bfn@myntet.se>
2020-01-29 17:48:35 +0200

    day 2 added

---
+++ Day2_dataTypes/Program.cs
@@ -0,0 +1,86 @@
+﻿using System;
+
+namespace Day2_dataTypes
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+ 
+
+
+        }
+
+        static void piemers()
+        {
+            int a = 5;
+            int b = 7;
+
+            int c = a + b;
+
+            Console.WriteLine("Ievadiet skaitli!");
+            String ievade = Console.ReadLine();
+
+            int parkonvertetsNr = Convert.ToInt32(ievade);
+            c = parkonvertetsNr - a;
+
+            Console.WriteLine("Skaitlis ir "+ c);
+
+            String virkne1 = "vertiba ";
+            String virkne2 = "ir ";
+
+            String virkne3 = virkne1 + virkne2;
+            Console.WriteLine(virkne3);
+        }
+
+        static void piemers2()
+        {
+            int a = 5;
+            int b = 6;
+
+            if (a > 5)
+            {
+                Console.WriteLine("ir");
+            }
+            else
+            {
+                Console.WriteLine("nav");
+            }
+
+            // > lielaks/mazaks
+            // < 
+            // <=
+            // >=
+            // == ( = ir pieskirt, == salidzinat)
+            // != nav vienads
+
+            String piem = "aaa";
+
+            if (piem == "aaa") //var salidzinat ari string- ir vienads vai nav
+            {
+
+            }
+
+            //int c = piem.Length; sadi varam dabut Stringa garumu
+        }
+
+        static void uzdevums1()
+        {
+            //Lietotajam paprasam kurs gads ir
+            Console.WriteLine("Kurs gads ir sobrid?");
+            //lietotajs ievada
+            String ievade = Console.ReadLine();
+            int pasreizejais = Convert.ToInt32(ievade);
+            //Lietotajam paprasat kura gads vins ir dzimis
+            Console.WriteLine("Kura gada esat dzimis?");
+
+            //lietotajs ievada
+            ievade = Console.ReadLine();
+            int dzimsanas = Convert.ToInt32(ievade);
+            //Atnemtas skaitlus lai izvaditu starpibu
+            int rezultats = pasreizejais - dzimsanas;
+            //Izvadam Vecums ir xx
+            Console.WriteLine("Jusu vecums ir " + rezultats);
+        }
+    }
+}
