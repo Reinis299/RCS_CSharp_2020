9cdf4ade44ab3b3541e5337176ce22fbc1b65a32
Martins <p998bfn@myntet.se>
2020-02-01 14:10:39 +0200

    uzd3 methods

---
+++ Day3_Methods/Program.cs
@@ -0,0 +1,102 @@
+﻿using System;
+
+namespace Day3_Methods
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            String a = piemers5();
+            Console.WriteLine(a);
+            /*piemers1();
+            piemers2();
+            piemers3(3,7);
+            int c = piemers4();*/
+
+
+            //1. Saskaitisana-
+            //ievads main metode, padodam skaitlus uz musu izveidoto metodi ka parametrus
+            //izvade notiek tur
+            Console.WriteLine("Ievadiet skaitli");
+            String ievade = Console.ReadLine();
+            int sk1 = Convert.ToInt32(ievade);
+
+            Console.WriteLine("Ievadiet skaitli");
+            ievade = Console.ReadLine();
+            int sk2 = Convert.ToInt32(ievade);
+
+            uzd1(sk1, sk2);
+
+            //2. Ievade notiek musu izveidota metode
+            //rezultatu atgreizam ar return un izvadam main
+
+            Console.WriteLine("Rezultats ir " + uzd2());
+
+            //3. Apvienojam 1 un 2.
+            // Musu izveidotaja metode notiek tikai darbiba
+            // visa ievade/izvade notiek main (gan parametri, gan return)
+
+            //Ievadi atstasu no 1. uzd
+
+            Console.WriteLine("Rezultats ir " + uzd3(sk1, sk2));
+        }
+
+        static void uzd1(int a, int b)
+        {
+            Console.WriteLine("rezultats ir " + (a + b));
+        }
+
+        static int uzd2()
+        {
+            Console.WriteLine("Ievadiet skaitli");
+            String ievade = Console.ReadLine();
+            int sk1 = Convert.ToInt32(ievade);
+
+            Console.WriteLine("Ievadiet skaitli");
+            ievade = Console.ReadLine();
+            int sk2 = Convert.ToInt32(ievade);
+
+            return sk1 + sk2;
+        }
+
+        static int uzd3(int a, int b)
+        {
+            return a + b;
+        }
+
+        static void piemers1()
+        {
+            Console.WriteLine("Parasta void metode");
+            piemers2();
+        }
+
+        static void piemers2()
+        {
+            //metodes obligati ir jaizsauc no main vai no citas metodes
+            Console.WriteLine("Velviena void metode");
+        }
+
+        static void piemers3(int a, int b)
+        {
+            Console.WriteLine(a + b);
+        }
+
+        static int piemers4()
+        {
+            int sk1 = 5;
+            int sk2 = 6;
+
+            return sk1 + sk2;
+        }
+
+        static String piemers5()
+        {
+            Console.WriteLine("ievadiet kaut ko");
+            String ievads = Console.ReadLine();
+
+            return ievads;
+        }
+
+
+    }
+}
--- day3_If/Program.cs
+++ day3_If/Program.cs
@@ -6,22 +6,78 @@ namespace day3_If
     {
         static void Main(string[] args)
         {
+            //Cilveks ievada tris skaitlus- int
+            //Jaizvada lielakais
+            Console.WriteLine("Ievadiet skaitli!");
+            String skaitlaIevade = Console.ReadLine();
+            int skaitlis1 = Convert.ToInt32(skaitlaIevade);
+
+            Console.WriteLine("Ievadiet skaitli!");
+            skaitlaIevade = Console.ReadLine();
+            int skaitlis2 = Convert.ToInt32(skaitlaIevade);
+
+            Console.WriteLine("Ievadiet skaitli!");
+            skaitlaIevade = Console.ReadLine();
+            int skaitlis3 = Convert.ToInt32(skaitlaIevade);
+
+
+            //1. variants
+            int biggest = 0;
+            if (skaitlis1 > skaitlis2)
+            {
+                biggest = skaitlis1;
+            }
+            else
+            {
+                biggest = skaitlis2;
+            }
+
+            if (biggest < skaitlis3)
+            {
+                biggest = skaitlis3;
+            }
+    
+
+            Console.WriteLine("Rezultats ir " + biggest);
+
+            //2
+ 
+             if (skaitlis1 >= skaitlis2 && skaitlis1 >= skaitlis3)
+             {
+                Console.WriteLine("Lielakais ir " + skaitlis1);
+             }
+             else if (skaitlis2 >= skaitlis1 && skaitlis2 >= skaitlis3)
+             {
+                Console.WriteLine("Lielakais ir " + skaitlis2);
+             }
+             else if(skaitlis3 >= skaitlis1 && skaitlis3 >= skaitlis1)
+             {
+                Console.WriteLine("Lielakais ir " + skaitlis3);
+             }
+            
+       
+
+        }
+
+        static void piemersZarosanas()
+        {
             int a = 5;
             int b = 6;
 
             // &&
             //jaizpildas abiem
-            if(a >= 5 && b == 6)
+            if (a >= 5 && b == 6)
             {
 
             }
 
             // ||
             //jaizpildas vienam
-            if(a > 5 || b == 6)
+            if (a > 5 || b == 6)
             {
 
             }
+
 
             int skaitlis = 5;
             //#
@@ -29,11 +85,11 @@ namespace day3_If
             //###
             //>5, <5, ==5
 
-            if(skaitlis > 5)
+            if (skaitlis > 5)
             {
                 Console.WriteLine("#");
             }
-            else if(skaitlis < 5)
+            else if (skaitlis < 5)
             {
                 Console.WriteLine("##");
             }
@@ -42,17 +98,49 @@ namespace day3_If
                 Console.WriteLine("###");
             }
 
+        }
+
+        static void uzd1()
+        {
+            //Kalkulators
+            //cilveks ievada divus skaitlus
+            Console.WriteLine("Ievadiet skaitli!");
+            String skaitlaIevade = Console.ReadLine();
+            Double skaitlis1 = Convert.ToDouble(skaitlaIevade);
+            //Cilveks ievada ko grib darit //+, -, *, /
 
 
-            //Gadijums ar vairakiem
-            //and ir viens nosacijums, or nakamais
-            //izpildas vai nu visi and vai viens or
-            int c = 7;
+            Console.WriteLine("Ievadiet skaitli!");
+            skaitlaIevade = Console.ReadLine();
+            Double skaitlis2 = Convert.ToDouble(skaitlaIevade);
 
-            if((a>5 && b<7 && c==7) || a < 9)
+            Console.WriteLine("Ievadiet darbibu: +,-,*,/");
+            String darbiba = Console.ReadLine();
+            //uz ekrana izvada rezultutu
+            //ja cilveks ka darbibu ievada jebko citu, nevis matematiskas, tad kludas pazinojums
+
+            if (darbiba == "+")
             {
-                
+                Console.WriteLine("Rezultats ir " + (skaitlis1 + skaitlis2));
             }
+            else if (darbiba == "-")
+            {
+                Console.WriteLine("Rezultats ir " + (skaitlis1 - skaitlis2));
+            }
+            else if (darbiba == "*")
+            {
+                Console.WriteLine("Rezultats ir " + (skaitlis1 * skaitlis2));
+            }
+            else if (darbiba == "/")
+            {
+                Console.WriteLine("Rezultats ir " + (skaitlis1 / skaitlis2));
+            }
+            else
+            {
+                Console.WriteLine("Kluda! Bija jaievada +, -, * vai /");
+            }
+
+            //Convert.ToDouble()
         }
     }
 }
