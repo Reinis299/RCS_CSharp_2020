8eafabf93a8741348b9d9442479aedb6a8f82582
Techbear <techbearHub@users.noreply.github.com>
2020-02-10 16:28:26 +0200

    added stuff

---
+++ breakSt/breakSt/Program.cs
@@ -0,0 +1,40 @@
+﻿using System;
+
+namespace breakSt
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            for(int i = 0; i < 6; i++)
+            {
+                Console.WriteLine("Ievadiet y vai n");
+                String input = Console.ReadLine();
+                
+                input = input.ToUpper(); //ToUpper, ToLower
+                if(input == "Y")
+                {
+                    break; //lai partrauktu darbibu ciklam
+                }
+                else
+                {
+                    Console.WriteLine(i);
+                }
+            }
+
+
+            while (true)
+            {
+                Console.WriteLine("aa");
+                String input = Console.ReadLine();
+
+                input = input.ToUpper(); //ToUpper, ToLower
+                if (input == "Y")
+                {
+                    break; //lai partrauktu darbibu ciklam
+                }
+
+            }
+        }
+    }
+}
