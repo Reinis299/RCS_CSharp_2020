05c5a46c0b58636146817b0ef9079b9d4c27f69f
Martins <p998bfn@myntet.se>
2020-02-15 11:08:45 +0200

    task 1 added

---
+++ Day7_klases_un_obj/Program.cs
@@ -0,0 +1,46 @@
+﻿using System;
+
+namespace Day7_klases_un_obj
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+
+            task1();
+
+
+            //Klases nosaukums ir trijsturis
+            //Cilveks ievada 3 malu vertibas - mainigie jaunaja klase
+            //Metodi, kas izvada perimetru mala + mala + mala
+            //visa ievade un izvade notiek no main- WriteLine un ReadLine
+            //ja kada mala ir 0, par to ir japazino
+        }
+
+        static void task1()
+        {
+            //uztaisat jaunu klasi ar nosaukumu SkaitluSumma
+            //Cilveks ievada 2 int tipa skaitlus no main
+            //uztaisat, get,set, un lai butu publiska metode, kas izvada
+            //abu skaitlu summu
+            //abiem skaitliem ir jabut pieejamiem visa SkaitluSumma klasē
+            //bet no Program.cs klases tikai ar get un set.
+            int sk1 = skaitlaIevade();
+            int sk2 = skaitlaIevade();
+
+            SkaitluSumma.setA(sk1);
+            SkaitluSumma.setB(sk2);
+
+            SkaitluSumma.Summa();
+
+        }
+
+        static int skaitlaIevade()
+        {
+            Console.WriteLine("Ievadiet skaitli!");
+            String skaitlaIevade = Console.ReadLine();
+            int skaitlis = Convert.ToInt32(skaitlaIevade);
+            return skaitlis;
+        }
+    }
+}
---
+++ Day7_klases_un_obj/SkaitluSumma.cs
@@ -0,0 +1,35 @@
+﻿using System;
+namespace Day7_klases_un_obj
+{
+    public class SkaitluSumma
+    {
+        private static int a = 0;
+        private static int b = 0;
+
+        public static void setA(int par1)
+        {
+            a = par1;
+        }
+
+        public static int getA()
+        {
+            return a;
+        }
+
+        public static void setB(int par1)
+        {
+            b = par1;
+        }
+
+        public static int getB()
+        {
+            return b;
+        }
+
+        public static void Summa()
+        {
+            Console.WriteLine("Summa ir " + (a + b));
+        }
+
+    }
+}
